2124b336f8cc74e61684e3fb01568f5ae586b296
Shrivastava <ssachin@amazon.com>
2017-11-29 23:59:29 -0500

    Diameter of tree

---
+++ LearnNCode/code/practice/treeDiameter/TreeDiameter.java
@@ -0,0 +1,27 @@
+package code.practice.treeDiameter;
+
+public class TreeDiameter {
+	
+	static int height(Node root) {
+		if(root == null) {
+			return 0;
+		}
+		return (1 + Math.max(height(root.left), height(root.right)));
+	}
+	
+	
+	static int diameter(Node root) {
+		if(root == null) {
+			return 0;
+		}
+		
+		int lheight = height(root.left);
+		int rheight = height(root.right);
+		
+		int ldiameter = diameter(root.left);
+		int rdiameter = diameter(root.right);
+		
+		return(Math.max(lheight+rheight+1, Math.max(ldiameter, rdiameter)));
+	}
+
+}
